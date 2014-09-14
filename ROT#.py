211c27b39511fbbdabe632bf789312539318fada
Jaken Herman <JakenHerman@outlook.com>
2014-09-13 22:08:23 -0500

    Update ROT#.py
    SHSU BITCHES

--- ROT#.py
+++ ROT#.py
@@ -1,1 +1,79 @@
-Some shit
+ciphertext = raw_input(">>> ")
+print ciphertext
+
+for letter in ciphertext:
+    if letter == 'a':
+        print 'z'
+    elif letter == 'b':
+        print 'y'
+    elif letter == 'c':
+        print 'x'
+    elif letter == 'd':
+        print 'w'
+    elif letter == 'e':
+        print 'v'
+    elif letter == 'f':
+        print 'u'
+    elif letter == 'g':
+        print 't'
+    elif letter == 'h':
+        print 's'
+    elif letter == 'i':
+        print 'r'
+    elif letter == 'j':
+        print 'q'
+    elif letter == 'k':
+        print 'p'
+    elif letter == 'l':
+        print 'o'
+    elif letter == 'm':
+        print 'n'
+    elif letter == 'n':
+        print 'm'
+    elif letter == 'o':
+        print 'l'
+    elif letter == 'p':
+        print 'k'
+    elif letter == 'q':
+        print 'j'
+    elif letter == 'r':
+        print 'i'
+    elif letter == 's':
+        print 'h'
+    elif letter == 't':
+        print 'g'
+    elif letter == 'u':
+        print 'f'
+    elif letter == 'v':
+        print 'e'
+    elif letter == 'w':
+        print 'd'
+    elif letter == 'x':
+        print 'c'
+    elif letter == 'y':
+        print 'b'
+    elif letter == 'z':
+        print 'a'
+    elif letter == '1':
+        print '4'
+    elif letter == '2':
+        print '5'
+    elif letter == '3':
+        print '6'
+    elif letter == '4':
+        print '7'
+    elif letter == '5':
+        print '8'
+    elif letter == '6':
+        print '9'
+    elif letter == '7':
+        print '0'
+    elif letter == '8':
+        print '1'
+    elif letter == '9':
+        print '2'
+    elif letter == '0':
+        print '3'
+    else:
+        print 'error'
+        
